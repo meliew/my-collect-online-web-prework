e2c2d928933184806afbbc8d346660c053e5e969
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-04 00:25:41 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  if block_given?
+    new_array = []
+    i = 0
+    while i < array.length
+      new_array.push(yield array[i])
+      i += 1
 
-
+    end
+    new_array
+  else
+    puts "No block was given"
+  end
+end
