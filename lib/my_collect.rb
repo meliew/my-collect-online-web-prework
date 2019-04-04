fce38ca53abcf54be204ce01169394fedf9e7105
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-04 00:09:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(array)
+  if block_given?
+    empty_array = []
+    counter = 0
+    while counter < array.length
+      yield (array[counter])
+      counter += 1
+      empty_array << counter
+    end
+    empty_array
+  else
+    puts "No block was given"
+  end
+end
